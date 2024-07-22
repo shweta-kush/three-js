6950e7a3209ca9d917d2914f96ad0543f4d639e8
Shweta-kush <kushishweta@gmail.com>
2024-07-22 22:14:38 +0530

    vid six

--- 06-cameras/src/script.js
+++ 06-cameras/src/script.js
@@ -1,4 +1,17 @@
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+
+
+/**cursor */
+const cursor={
+    x:0,y:0
+}
+
+window.addEventListener('mousemove', (event) =>{
+    cursor.x = event.clientX / sizes.width -0.5
+    cursor.y = - (event.clientY / sizes.height -0.5)
+    // console.log(cursor.y)
+})
 
 /**
  * Base
@@ -9,7 +22,7 @@ const canvas = document.querySelector('canvas.webgl')
 // Sizes
 const sizes = {
     width: 800,
-    height: 600
+    height: 500
 }
 
 // Scene
@@ -23,14 +36,19 @@ const mesh = new THREE.Mesh(
 scene.add(mesh)
 
 // Camera
-// const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-const aspectRatio = sizes.width / sizes.height
-const camera= new THREE.OrthographicCamera(-1* aspectRatio, 1* aspectRatio, 1, -1, 0.1, 100)
-camera.position.x = 2
-camera.position.y = 2
-camera.position.z = 2
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+// const aspectRatio = sizes.width / sizes.height
+// const camera= new THREE.OrthographicCamera(-1* aspectRatio, 1* aspectRatio, 1, -1, 0.1, 100)
+// camera.position.x = 2
+// camera.position.y = 2
+camera.position.z = 4
 camera.lookAt(mesh.position)
 scene.add(camera)
+
+/**control */
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
@@ -43,11 +61,23 @@ const clock = new THREE.Clock()
 
 const tick = () =>
 {
-    const elapsedTime = clock.getElapsedTime()
+    // const elapsedTime = clock.getElapsedTime()
 
     // Update objects
-    mesh.rotation.y = elapsedTime;
+    // mesh.rotation.y = elapsedTime;
 
+    // update camera
+    // camera.position.x=cursor.x *3
+    // camera.position.y=cursor.y *3
+
+    // camera.position.x = Math.sin(cursor.x * Math.PI * 2) *4
+    // camera.position.z = Math.cos(cursor.x * Math.PI * 2) *4
+    // camera.position.y = cursor.y *5
+    // camera.lookAt(new THREE.Vector3())
+
+
+    /**update control for orbotcontrols */
+    controls.update()
     // Render
     renderer.render(scene, camera)
 
